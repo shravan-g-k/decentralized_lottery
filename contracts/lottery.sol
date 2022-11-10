31154b93b25ba3cfe67863b1feb7f72e34378c62
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-10 22:48:17 +0530

    added onlyOwner modifier

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery  is Ownable{
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
 
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+
     constructor(address _priceFeedAddress) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
-        require(msg.value >= getEntranceFee(),"Not enough ETH!!");
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!!");
         players.push(payable(msg.sender));
     }
 
@@ -24,7 +35,13 @@ contract Lottery {
         return costToEnter;
     }
 
-    function srtartLottery() public {}
+    function srtartLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Cant start lottery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
     function endLottery() public {}
 }
